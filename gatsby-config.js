b4c13803f7a6db611e853868f842b0c6a6f14c5b
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-27 20:58:04 -0500

    Update config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `J. Patrick Fulton - Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `J. Patrick Fulton`,
+      summary: `who lives and works in Chicago occasionally playing pool.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `A personal blog on occasionally useful technical topics.`,
+    siteUrl: `https://blog.jpatrickfulton.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jpatrickfulton`,
     },
   },
   plugins: [
@@ -102,7 +102,7 @@ module.exports = {
               }
             }`,
             output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
+            title: "J. Patrick Fulton Blog RSS Feed",
           },
         ],
       },
@@ -110,8 +110,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `J. Patrick Fulton - Blog`,
+        short_name: `Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
