d4804b242246601117283da547fa1a5ff6e8836c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-28 00:39:02 -0500

    Update footer.

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()} J. Patrick Fulton, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
