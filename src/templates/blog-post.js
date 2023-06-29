10a4e8de9658924902fec0f8c27827e782c458d9
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-29 00:11:45 -0500

    Add next and previous links.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,7 +11,7 @@ import { rhythm, scale } from "../utils/typography"
 function BlogPostTemplate({ pageContext, data: { mdx, site }, children }) {
   const post = mdx
   const siteTitle = site.siteMetadata.title
-  // const { previous, next } = pageContext
+  const { previous, next } = pageContext
 
   return (
     <Layout location={pageContext.location} title={siteTitle}>
@@ -40,7 +40,30 @@ function BlogPostTemplate({ pageContext, data: { mdx, site }, children }) {
       />
       <Bio />
 
-      
+      <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={`/blog${previous.fields.slug}`} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={`/blog${next.fields.slug}`} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
     </Layout>
   )
 }
