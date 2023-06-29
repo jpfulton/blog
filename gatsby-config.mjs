4c411f19088a26249411fd4797dc6cc45618211c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-29 14:58:37 -0500

    Make app pwa friendly.

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -132,9 +132,12 @@ const config = {
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#ffffff`,
         display: `standalone`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon_options: {
+          purpose: `any maskable`,
+        },
       },
     },
     {
