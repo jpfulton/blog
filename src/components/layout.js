01966ba5c329bd7837d1a7c5b7ceb919d4833c05
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-29 13:44:22 -0500

    Remove gatsby link. Will write an article for them later.

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()} J. Patrick Fulton, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        © {new Date().getFullYear()} J. Patrick Fulton
       </footer>
     </div>
   );
