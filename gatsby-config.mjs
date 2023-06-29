eba3a44a3e8d67b6ad38b3a20f416c6862d167fe
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-29 11:22:42 -0500

    Modernize mdx plugin configuration.

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -74,23 +74,31 @@ const config = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 630,
+        mdxOptions: {
+          remarkPlugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 630,
+              },
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+            {
+              resolve: `gatsby-remark-responsive-iframe`,
+              options: {
+                wrapperStyle: `margin-bottom: 1.0725rem`,
+              },
             },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
+            {
+              resolve: `gatsby-remark-prismjs`,
+            },
+            {
+              resolve: `gatsby-remark-copy-linked-files`,
+            },
+            {
+              resolve: `gatsby-remark-smartypants`,
+            },
+          ],
+        },
       },
     },
     {
