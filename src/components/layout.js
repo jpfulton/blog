fcd5de5334fe379192f5d7675c3c3d08f180b2b0
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-07-01 17:42:02 -0500

    Update stylings to make mobile more obvious.

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ const Layout = ({ location, title, children }) => {
   } else {
     header = (
       <Link className="header-link-home" to="/">
-        {title}
+        ← {title}
       </Link>
     );
   }
