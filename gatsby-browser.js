a6f91df68264d7c5d928507e5582f986891163ad
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-07-04 12:03:44 -0500

    Update with import ordering and note on import order.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,21 @@
+/* NOTE: CSS and SASS file import order matters here */
+
 // normalize CSS across browsers
 import "./src/styles/normalize.css";
-// custom CSS styles
+
+// global styles
 import "./src/styles/global-style.scss";
+
+// prismjs highlighting theme for code blocks
+import "prismjs/themes/prism-coy.css";
+// prismjs highlighting plugins for code blocks
+import "prismjs/plugins/command-line/prism-command-line.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+
+// custom component and page level styles
 import "./src/styles/bio.scss";
 import "./src/styles/blog.scss";
 import "./src/styles/cookie-consent.scss";
 import "./src/styles/post-summary.scss";
-import "./src/styles/tags.scss";
 import "./src/styles/prism.scss";
-
-// Highlighting for code blocks
-import "prismjs/themes/prism-coy.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import "prismjs/plugins/command-line/prism-command-line.css";
+import "./src/styles/tags.scss";
