756295c89639f6b0558702eaadc94d815bc6d26a
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-07-12 15:31:41 -0500

    Document script.

--- .github/scripts/indexnow.sh
+++ .github/scripts/indexnow.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
+# Configurable variables used in IndexNow call and URL construction
 HOST="www.jpatrickfulton.dev"
 BASE_URL="https://${HOST}"
 BING_API_KEY="8846484b349642449a66629f496422f8"
 BING_URL_TO_KEY_FILE="${BASE_URL}/${BING_API_KEY}.txt"
 BING_URL="https://www.bing.com"
 
-BLOG_DIR=../../../content/blog/;
+BLOG_DIR=../../../content/blog/; # Location of /content/blog/ folder relative to tmp working dir
+
 WORKING_DIR=$( pwd; );
 TMP_DIR=$WORKING_DIR"/tmp";
-
 JSON_FILE=body.json;
 
+# Create and enter the working directory if it does not exist
 if [ ! -d $TMP_DIR ]
   then
     echo "Creating temporary working directory.";
     mkdir $TMP_DIR;
 fi
-
 cd $TMP_DIR;
 
 echo "Building POST body from repository structure.";
@@ -26,6 +27,7 @@ if [ -f $JSON_FILE ]
     rm $JSON_FILE;
 fi
 
+# Begin JSON body construction
 touch $JSON_FILE;
 echo "{" >> $JSON_FILE;
 echo "  \"host\": \"${HOST}\"," >> $JSON_FILE;
@@ -33,8 +35,11 @@ echo "  \"key\": \"${BING_API_KEY}\"," >> $JSON_FILE;
 echo "  \"keyLocation\": \"${BING_URL_TO_KEY_FILE}\"," >> $JSON_FILE;
 echo "  \"urlList\": [" >> $JSON_FILE;
 
+# Add the root URL
 echo "    \"${BASE_URL}/\"," >> $JSON_FILE;
 
+# Iterate over folders in the /content/blog/ directory
+# to create links to each individual generated blog page
 BLOGS=($(ls $BLOG_DIR));
 for BLOG in "${BLOGS[@]}"
 do
@@ -43,6 +48,7 @@ done
 
 echo "    ]" >> $JSON_FILE;
 echo "}" >> $JSON_FILE;
+# JSON body constrction complete
 
 echo "JSON POST body contents:";
 cat $JSON_FILE;
@@ -52,6 +58,7 @@ echo "---";
 echo "Updating Bing through POST to /IndexNow...";
 echo;
 
+# Use curl to POST the JSON content to Bing IndexNow endpoint
 curl ${BING_URL}/IndexNow \
   -H "Content-Type: application/json; charset=utf-8" \
   -d "$(cat $JSON_FILE)" \
