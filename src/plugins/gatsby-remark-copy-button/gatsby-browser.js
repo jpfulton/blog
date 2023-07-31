c466b79646beaa3e2b58e0fb6990d1ea7ed0c1b8
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-07-31 13:37:35 -0500

    Prevent repeat clicks during copy operation.

--- src/plugins/gatsby-remark-copy-button/gatsby-browser.js
+++ src/plugins/gatsby-remark-copy-button/gatsby-browser.js
@@ -2,17 +2,44 @@ import "./styles.scss";
 
 export function onClientEntry() {
   window.copyToClipboard = (str, button) => {
+    if (!button) return;
+
+    // prevent multiple immediate clicks on this operation
+    const lockAttributeName = "data-copy-button-in-progress";
+    if (button.getAttribute(lockAttributeName) === null)
+      button.setAttribute(lockAttributeName, "false");
+
+    if (button.getAttribute(lockAttributeName) === "true") return;
+
     const copyIcon = button.querySelector(".copy-icon");
     const checkIcon = button.querySelector(".check-icon");
 
-    navigator.clipboard.writeText(str).then(() => {
-      copyIcon.style.display = "none";
-      checkIcon.style.display = "block";
+    if (button.getAttribute(lockAttributeName) === "false") {
+      // set tag local lock variable
+      button.setAttribute(lockAttributeName, "true");
 
-      setTimeout(() => {
-        checkIcon.style.display = "none";
-        copyIcon.style.display = "block";
-      }, 2000);
-    });
+      navigator.clipboard.writeText(str).then(
+        () => {
+          // clipboard success operation
+          copyIcon.style.display = "none";
+          checkIcon.style.display = "block";
+
+          setTimeout(() => {
+            checkIcon.style.display = "none";
+            copyIcon.style.display = "block";
+
+            // unset tag local lock variable
+            button.setAttribute(lockAttributeName, "false");
+          }, 2000);
+        },
+        () => {
+          // clipboard failure operation
+          console.log("Failed to copy content to clipboard.");
+
+          // unset tag local lock variable
+          button.setAttribute(lockAttributeName, "false");
+        }
+      );
+    }
   };
 }
