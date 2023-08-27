cabc7cc8ca36720cb169c29897afffeb0db1d735
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-27 16:14:23 -0500

    Expand logic to support desired UX.

--- src/components/headerMenu.js
+++ src/components/headerMenu.js
@@ -3,13 +3,34 @@ import React from "react";
 
 export const HeaderMenu = () => {
   let isOpen = false;
+  let linkFromMouseDown = null;
 
-  const menuToggleFunc = () => {
+  const menuToggleFunc = (event) => {
+    const menuTag = document.querySelector("#headerMenu");
     const container = document.querySelector("#headerMenuContainer");
+
+    /*
+    if (event) {
+      console.log("Event: %o", event);
+    }
+    if (event && event.target) {
+      console.log("Target: %o", event.target);
+    }
+    if (event && event.relatedTarget) {
+      console.log("Related Target: %o", event.relatedTarget);
+    }
+    */
+
+    if (linkFromMouseDown) {
+      // console.log("Link component from mouse down: %o", linkFromMouseDown);
+      linkFromMouseDown.click();
+    }
+
     if (isOpen) {
       container.style.display = "none";
       isOpen = false;
     } else {
+      menuTag.focus();
       container.style.display = "block";
       isOpen = true;
     }
@@ -18,32 +39,39 @@ export const HeaderMenu = () => {
   const onKeyMenuToggle = (keydownEvent) => {
     const KEY_CODE_ENTER = 13;
     if (keydownEvent.code) {
-      if (keydownEvent.code === KEY_CODE_ENTER) menuToggleFunc();
+      if (keydownEvent.code === KEY_CODE_ENTER) menuToggleFunc(keydownEvent);
       //   if (isOpen && keydownEvent.code === 27) menuToggleFunc();
     }
   };
 
   const onLoseFocusMenu = (event) => {
     const container = document.querySelector("#headerMenu");
+
     if (container.contains(event.relatedTarget)) {
       return;
     }
     if (isOpen) {
-      menuToggleFunc();
+      menuToggleFunc(event);
     }
   };
 
+  const onLinkMouseDown = (event) => {
+    // console.log("Mouse down event: %o", event);
+    linkFromMouseDown = event.target;
+  };
+
   return (
-    <div id="headerMenu" onBlur={onLoseFocusMenu}>
-      <button
-        id="headerMenuIcon"
-        onClick={menuToggleFunc}
-        onKeyDown={onKeyMenuToggle}
-      >
+    <button
+      id="headerMenu"
+      onClick={menuToggleFunc}
+      onKeyDown={onKeyMenuToggle}
+      onBlur={onLoseFocusMenu}
+    >
+      <div id="headerMenuIcon">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           height="32px"
-          style={{ "enable-background": "new 0 0 32 32;" }}
+          style={{ enableBackground: "new 0 0 32 32;" }}
           version="1.1"
           viewBox="0 0 32 32"
           width="32px"
@@ -53,21 +81,29 @@ export const HeaderMenu = () => {
         <div id="headerMenuContainer">
           <ol>
             <li>
-              <Link to="/profile/">Author Profile</Link>
+              <Link to="/profile/" onMouseDown={onLinkMouseDown}>
+                Author Profile
+              </Link>
             </li>
             <li>
-              <Link to="/cookie-policy/">Cookie Policy</Link>
+              <Link to="/cookie-policy/" onMouseDown={onLinkMouseDown}>
+                Cookie Policy
+              </Link>
             </li>
             <li>
-              <Link to="/privacy-policy/">Privacy Policy</Link>
+              <Link to="/privacy-policy/" onMouseDown={onLinkMouseDown}>
+                Privacy Policy
+              </Link>
             </li>
             <li>
-              <Link to="/terms-of-use/">Terms of Use</Link>
+              <Link to="/terms-of-use/" onMouseDown={onLinkMouseDown}>
+                Terms of Use
+              </Link>
             </li>
           </ol>
         </div>
-      </button>
-    </div>
+      </div>
+    </button>
   );
 };
 
