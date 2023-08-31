9e75a2a146c9060a50a078612c76e9e277655c80
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-31 11:36:03 -0500

    Tweaks to gtag and outbound link logging.

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -277,13 +277,13 @@ const config = {
         // This config will be shared across all trackingIds
         gtagConfig: {
           //optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
+          //anonymize_ip: true,
           cookie_expires: 0,
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
