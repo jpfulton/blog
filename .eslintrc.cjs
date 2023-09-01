7056fb1503bd94ae321ea396662fc902df720155
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-01 16:06:56 -0500

    Turn off some rules for issues that won't be corrected.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,4 +5,8 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   root: true,
   ignorePatterns: ["public/**/*", "node_modules/**/*", ".cache/**/*"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+  },
 };
